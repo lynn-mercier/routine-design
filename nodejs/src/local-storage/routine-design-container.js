fb0e15f9c6359e9c7b782ac142a90c0e0a63e2dd
Lynn Mercier <44537076+lynn-mercier@users.noreply.github.com>
2019-02-10 16:44:53 -0800

    fix: make RoutineDesignContainer more robust to failed commands on Docker (#89)

--- nodejs/src/local-storage/routine-design-container.js
+++ nodejs/src/local-storage/routine-design-container.js
@@ -18,25 +18,53 @@ class RoutineDesignContainer {
     const volumeFlag = '-v '+process.env.PWD+':/home/routine-design';
     const nameFlag = '--name='+this.containerName_;
     const sysAdminFlag = '--cap-add=SYS_ADMIN';
-    const startContainerPromise =
-      this.docker_.command('create -t '+volumeFlag+' '+nameFlag+' '+sysAdminFlag+' routine-design').then(() => {
-        return this.docker_.command('start '+this.containerName_);
-      });
-    await this.localDirectory_.create();
-    const writeGoogleCredsPromise = new Promise((resolve, reject) => {
-      this.myFs_.writeFile(this.googleCredentialsPath_, process.env.ROUTINE_DESIGN_GOOGLE_CREDS, function(err) {
+    const createContainerPromise = new Promise((resolve, reject) => {
+      this.docker_.command('create -t '+volumeFlag+' '+nameFlag+' '+sysAdminFlag+' routine-design', function(err) {
         if (err) {
-          reject(err);
+          const stderr = err.substring(err.indexOf('stderr = \'')+'stderr = \''.length, err.length-2);
+          reject(new Error(stderr));
         } else {
           resolve();
         }
+      });
+    });
+    const startContainerPromise = createContainerPromise.then(() => {
+      return new Promise((resolve, reject) => {
+        this.docker_.command('start '+this.containerName_, function(err) {
+          if (err) {
+            const stderr = err.substring(err.indexOf('stderr = \'')+'stderr = \''.length, err.length-2);
+            reject(new Error(stderr));
+          } else {
+            resolve();
+          }
+        });
+      });
+    });
+    const writeGoogleCredsPromise = this.localDirectory_.create().then(() => {
+      return new Promise((resolve, reject) => {
+        this.myFs_.writeFile(this.googleCredentialsPath_, process.env.ROUTINE_DESIGN_GOOGLE_CREDS, function(err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
       });
     });
     return Promise.all([startContainerPromise, writeGoogleCredsPromise]);
   }
 
   async buildNodeSass() {
-    return this.docker_.command('exec '+this.containerName_+' bash -c "npm rebuild node-sass"');
+    return new Promise((resolve, reject) => {
+      this.docker_.command('exec '+this.containerName_+' bash -c "npm rebuild node-sass"', function(err, data) {
+        if (err) {
+          const stderr = err.substring(err.indexOf('stderr = \'')+'stderr = \''.length, err.length-2);
+          reject(new Error(stderr));
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 
   async run(command, myUsername = username, myUserid = userid) {
@@ -44,12 +72,49 @@ class RoutineDesignContainer {
       return '-u '+myUserid.uid(usernameResult);
     });
     const googleEnvironmentFlag = '-e GOOGLE_APPLICATION_CREDENTIALS="'+path.join('/home/routine-design', this.googleCredentialsPath_)+'"';
-    return this.docker_.command('exec '+(await userFlagPromise)+' '+googleEnvironmentFlag+' '+this.containerName_+' bash -c "'+command+'"');
+    
+    const dockerCommandPromise = userFlagPromise.then((userFlag) => {
+      return 'exec '+userFlag+' '+googleEnvironmentFlag+' '+this.containerName_+' bash -c "'+command+'"';
+    });
+
+    return dockerCommandPromise.then((dockerCommand) => {
+      return new Promise((resolve, reject) => {
+        this.docker_.command(dockerCommand, function(err, data) {
+          if (err) {
+            const stderr = err.substring(err.indexOf('stderr = \'')+'stderr = \''.length, err.length-2);
+            console.error(stderr);
+            const errorMessage = stderr.substring(stderr.indexOf(':')+2, stderr.indexOf('\n'));
+            reject(new Error(errorMessage));
+          } else {
+            resolve(data);
+          }
+        });
+      });
+    });
   }
 
   async cleanup() {
-    const removeContainerPromise = this.docker_.command('stop '+this.containerName_).then(() => {
-      return this.docker_.command('rm '+this.containerName_);
+    const stopContainerPromise = new Promise((resolve, reject) => {
+      this.docker_.command('stop '+this.containerName_, function(err) {
+        if (err) {
+          const stderr = err.substring(err.indexOf('stderr = \'')+'stderr = \''.length, err.length-2);
+          reject(new Error(stderr));
+        } else {
+          resolve();
+        }
+      });
+    });
+    const removeContainerPromise = stopContainerPromise.then(() => {
+      return new Promise((resolve, reject) => {
+        this.docker_.command('rm '+this.containerName_, function(err) {
+          if (err) {
+            const stderr = err.substring(err.indexOf('stderr = \'')+'stderr = \''.length, err.length-2);
+            reject(new Error(stderr));
+          } else {
+            resolve();
+          }
+        });
+      });
     });
     const deleteGoogleCredsPromise = new Promise((resolve, reject) => {
       this.myFs_.unlink(this.googleCredentialsPath_, function(err) {
--- nodejs/test/local-storage/routine-design-container.test.js
+++ nodejs/test/local-storage/routine-design-container.test.js
@@ -11,53 +11,129 @@ describe('local-storage/RoutineDesignContainer', function() {
   process.env.PWD = '/home';
   process.env.ROUTINE_DESIGN_GOOGLE_CREDS = 'routine-design-google-creds';
   td.when(LocalDirectory.prototype.getFilePath(td.matchers.anything())).thenReturn('auth.json');
+  td.when(LocalDirectory.prototype.create()).thenResolve();
   describe('successful filesystem', function() {
     const successfulFs = td.object(fs);
     td.when(successfulFs.writeFile(td.matchers.anything(), td.matchers.anything())).thenCallback();
     td.when(successfulFs.unlink(td.matchers.anything())).thenCallback();
-    it('#start', async function() {
-      const MockDocker = td.constructor(Docker);
-      const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
-      td.when(MockDocker.prototype.command(td.matchers.anything())).thenResolve();
-      return container.start().then(function() {
-        const dockerCalls = td.explain(MockDocker.prototype.command).calls;
-        expect(dockerCalls.length).to.equal(2);
-        expect(dockerCalls[0].args[0]).to.equal('create -t -v /home:/home/routine-design --name=foo --cap-add=SYS_ADMIN routine-design');
-        expect(dockerCalls[1].args[0]).to.equal('start foo');
-        expect(td.explain(LocalDirectory.prototype.create).calls.length).to.equal(1);
-        expect(td.explain(successfulFs.writeFile).calls[0].args[0]).to.equal('auth.json');
-        expect(td.explain(successfulFs.writeFile).calls[0].args[1]).to.equal('routine-design-google-creds');
+    describe('#start', function() {
+      it('successful docker', async function() {
+        const MockDocker = td.constructor(Docker);
+        const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
+        td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback();
+        return container.start().then(function() {
+          const dockerCalls = td.explain(MockDocker.prototype.command).calls;
+          expect(dockerCalls.length).to.equal(2);
+          expect(dockerCalls[0].args[0]).to.equal('create -t -v /home:/home/routine-design --name=foo --cap-add=SYS_ADMIN routine-design');
+          expect(dockerCalls[1].args[0]).to.equal('start foo');
+          expect(td.explain(LocalDirectory.prototype.create).calls.length).to.equal(1);
+          expect(td.explain(successfulFs.writeFile).calls[0].args[0]).to.equal('auth.json');
+          expect(td.explain(successfulFs.writeFile).calls[0].args[1]).to.equal('routine-design-google-creds');
+        });
+      });
+      it('unsuccessful docker', async function() {
+        const MockDocker = td.constructor(Docker);
+        const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
+        td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback(`error: 'Error: Command failed: docker blah blah
+Cannot connect to the Docker daemon at unix:///var/run/docker.sock. Is the docker daemon running?
+' stdout = '' stderr = 'Cannot connect to the Docker daemon at unix:///var/run/docker.sock. Is the docker daemon running?
+'`);
+        let caughtError = false;
+        try {
+          await container.start()
+        } catch (error) {
+          caughtError = true;
+          expect(error.message).to.equal('Cannot connect to the Docker daemon at unix:///var/run/docker.sock. Is the docker daemon running?');
+        }
+        expect(caughtError).to.equal(true);
       });
     });
-    it('#buildNodeSass', async function() {
-      const MockDocker = td.constructor(Docker);
-      const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
-      td.when(MockDocker.prototype.command(td.matchers.anything())).thenResolve();
-      return container.buildNodeSass().then(function() {
-        expect(td.explain(MockDocker.prototype.command).calls[0].args[0]).to.equal('exec foo bash -c "npm rebuild node-sass"');
+    describe('#buildNodeSass', function() {
+      it('successful docker', async function() {
+        const MockDocker = td.constructor(Docker);
+        const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
+        td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback();
+        return container.buildNodeSass().then(function() {
+          expect(td.explain(MockDocker.prototype.command).calls[0].args[0]).to.equal('exec foo bash -c "npm rebuild node-sass"');
+        });
+      });
+      it('unsuccessful docker', async function() {
+        const MockDocker = td.constructor(Docker);
+        const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
+        td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback(`error: 'Error: Command failed: docker blah blah
+Cannot build Sass
+' stdout = '' stderr = 'Cannot build Sass
+'`);
+        let caughtError = false;
+        try {
+          await container.buildNodeSass()
+        } catch (error) {
+          caughtError = true;
+          expect(error.message).to.equal('Cannot build Sass');
+        }
+        expect(caughtError).to.equal(true);
       });
     });
-    it('#run', async function() {
-      const MockDocker = td.constructor(Docker);
-      const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
-      td.when(MockDocker.prototype.command(td.matchers.anything())).thenResolve();
+    describe('#run', function() {
       td.when(username()).thenResolve('username');
       td.when(userid.uid(td.matchers.anything())).thenReturn(1234);
-      return container.run('command', username, userid).then(function() {
-        expect(td.explain(MockDocker.prototype.command).calls[0].args[0]).to.equal('exec -u 1234 -e GOOGLE_APPLICATION_CREDENTIALS="/home/routine-design/auth.json" foo bash -c "command"');
-        expect(td.explain(userid.uid).calls[0].args[0]).to.equal('username');
+      it('successful docker', async function() {
+        const MockDocker = td.constructor(Docker);
+        const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
+        td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback(null, 'data');
+        return container.run('command', username, userid).then(function(data) {
+          expect(data).to.equal('data');
+          expect(td.explain(MockDocker.prototype.command).calls[0].args[0]).to.equal('exec -u 1234 -e GOOGLE_APPLICATION_CREDENTIALS="/home/routine-design/auth.json" foo bash -c "command"');
+          expect(td.explain(userid.uid).calls[0].args[0]).to.equal('username');
+        });
+      });
+      it('unsuccessful docker', async function() {
+        const MockDocker = td.constructor(Docker);
+        const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
+        td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback(`error: 'Error: Command failed: docker blah blah
+Error: Cannot run
+  at some line
+' stdout = '' stderr = 'Error: Cannot run
+  at some line
+'`);
+        let caughtError = false;
+        try {
+          await container.run('command', username, userid);
+        } catch(error) {
+          caughtError = true;
+          expect(error.message).to.equal('Cannot run');
+        }
+        expect(caughtError).to.equal(true);
       });
     });
-    it('#cleanup', async function() {
-      const MockDocker = td.constructor(Docker);
-      const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
-      td.when(MockDocker.prototype.command(td.matchers.anything())).thenResolve();
-      return container.cleanup().then(function() {
-        const dockerCalls = td.explain(MockDocker.prototype.command).calls;
-        expect(dockerCalls.length).to.equal(2);
-        expect(dockerCalls[0].args[0]).to.equal('stop foo');
-        expect(dockerCalls[1].args[0]).to.equal('rm foo');
-        expect(td.explain(successfulFs.unlink).calls[0].args[0]).to.equal('auth.json');
+    describe('#cleanup', function() {
+      it('successful docker', async function() {
+        const MockDocker = td.constructor(Docker);
+        const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
+        td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback();
+        return container.cleanup().then(function() {
+          const dockerCalls = td.explain(MockDocker.prototype.command).calls;
+          expect(dockerCalls.length).to.equal(2);
+          expect(dockerCalls[0].args[0]).to.equal('stop foo');
+          expect(dockerCalls[1].args[0]).to.equal('rm foo');
+          expect(td.explain(successfulFs.unlink).calls[0].args[0]).to.equal('auth.json');
+        });
+      });
+      it('unsuccessful docker', async function() {
+        const MockDocker = td.constructor(Docker);
+        const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, successfulFs);
+        td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback(`error: 'Error: Command failed: docker blah blah
+Cannot stop docker
+' stdout = '' stderr = 'Cannot stop docker
+'`);
+        let caughtError = false;
+        try {
+          await container.cleanup();
+        } catch(error) {
+          caughtError = true;
+          expect(error.message).to.equal('Cannot stop docker');
+        }
+        expect(caughtError).to.equal(true);
       });
     });
   });
@@ -68,7 +144,7 @@ describe('local-storage/RoutineDesignContainer', function() {
     it('#start', async function() {
       const MockDocker = td.constructor(Docker);
       const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, unsuccessfulFs);
-      td.when(MockDocker.prototype.command(td.matchers.anything())).thenResolve();
+      td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback();
       let caughtError = false;
       try {
         await container.start();
@@ -80,7 +156,7 @@ describe('local-storage/RoutineDesignContainer', function() {
     it('#cleanup', async function() {
       const MockDocker = td.constructor(Docker);
       const container = new RoutineDesignContainer('foo', MockDocker, LocalDirectory, unsuccessfulFs);
-      td.when(MockDocker.prototype.command(td.matchers.anything())).thenResolve();
+      td.when(MockDocker.prototype.command(td.matchers.anything())).thenCallback();
       let caughtError = false;
       try {
         await container.cleanup();
